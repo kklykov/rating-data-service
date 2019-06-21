b64cb4e58395beae6cad42d28f1a82f867eba1d3
Konstantin Klykov <kklykov@imbee.me>
2019-06-21 22:10:18 +0200

    Initial Commit. Basic Spring io start project with web dependencies.

---
+++ src/main/java/io/klykov/ratingdataservice/RatingDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.klykov.ratingdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingDataServiceApplication.class, args);
+	}
+
+}
